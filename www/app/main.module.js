9dec27dda44579fd6c90f499be1d3df050fdcb67
Lingke <wlingke@gmail.com>
2014-12-24 16:31:10 -0500

    chat app

--- www/app/main.module.js
+++ www/app/main.module.js
@@ -1,23 +1,17 @@
-angular.module('starter', ['ionic'])
+var APP = angular.module('Chat', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('my_chats', {
+                url: "/",
+                template: "<ion-view><ion-content>My Chats</ion-content></ion-view>"
+            })
+            .state('create_chat', {
+                url: "/create",
+                template: "<ion-view><ion-content>Create Chat</ion-content></ion-view>"
+            })
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/');
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
+    });
 
